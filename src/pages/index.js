b27abb1e6bf13d4345436cde7aa4ac01a56cc4a9
Catherina36 <79195881+Catherina36@users.noreply.github.com>
2024-04-06 23:38:37 -0700

    Delete src directory

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Profile } from './Profile';
-export { default as ProgramDetails } from './ProgramDetails';
-export { default as CreateProgram } from './CreateProgram';
